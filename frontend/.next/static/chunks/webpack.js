cad1cf732e8f47a2ae6f8e68c4ab4622f63f9251
mohtashimnawaz <mohtashim3nawaz@gmail.com>
2025-12-19 01:45:54 +0530

    color scheme

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8a9c78eeb0b4352"; }
+/******/ 		__webpack_require__.h = function() { return "9fd3546d2797272a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
